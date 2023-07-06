69be4cb4256a3bb745e777ee8e1a89a69de75e25
Juri Pakaste <juri.pakaste@richie.fi>
2023-07-06 17:15:23 +0300

    Try harder at macOS version compatibility
    Only add Benchmark if running on macOS 13 or later.

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
 // swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+import Foundation
 import PackageDescription
 
 let package = Package(
     name: "Parse3339",
-    platforms: [.macOS(.v13)],
     products: [
         .library(
             name: "Parse3339",
@@ -24,17 +24,21 @@ let package = Package(
     ]
 )
 
-// Benchmark of ParserBenchmarks
-package.targets += [
-    .executableTarget(
-        name: "ParserBenchmarks",
-        dependencies: [
-            .product(name: "Benchmark", package: "package-benchmark"),
-            "Parse3339",
-        ],
-        path: "Benchmarks/ParserBenchmarks",
-        plugins: [
-            .plugin(name: "BenchmarkPlugin", package: "package-benchmark"),
-        ]
-    ),
-]
+#if os(macOS)
+if ProcessInfo.processInfo.isOperatingSystemAtLeast(.init(majorVersion: 13, minorVersion: 0, patchVersion: 0)) {
+    package.platforms = [.macOS(.v13)]
+    package.targets += [
+        .executableTarget(
+            name: "ParserBenchmarks",
+            dependencies: [
+                .product(name: "Benchmark", package: "package-benchmark"),
+                "Parse3339",
+            ],
+            path: "Benchmarks/ParserBenchmarks",
+            plugins: [
+                .plugin(name: "BenchmarkPlugin", package: "package-benchmark"),
+            ]
+        ),
+    ]
+}
+#endif
